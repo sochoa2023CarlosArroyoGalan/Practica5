9219b1301c00281bde1947fd02770f41665beaa5
Carlos <erentitan4512@gmail.com>
2023-11-13 17:10:24 +0100

    Parte 2

---
+++ app/src/main/java/net/iessochoa/carlosarroyogalan/practica5/model/Tarea.kt
@@ -0,0 +1,15 @@
+package net.iessochoa.carlosarroyogalan.practica5.model
+
+data class Tarea(
+    var id:Long?=null,//id único
+    val categoria:Int,
+    val prioridad:Int,
+    val pagado:Boolean,
+    val estado:Int,
+    val horasTrabajo:Int,
+    val valoracionCliente:Float,
+    val tecnico:String,
+    val descripcion:String
+) {
+}
+
