8dfdc3c01234b778d36fa0e2c6ec6d66957795d1
erentitan4512 <erentitan4512@gmail.com>
2024-02-23 22:22:10 +0100

    Inicio Parte 7

--- app/src/main/java/net/iessochoa/carlosarroyogalan/practica5/ui/SettingsFragment.kt
+++ app/src/main/java/net/iessochoa/carlosarroyogalan/practica5/ui/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package net.iessochoa.carlosarroyogalan.practica5.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import net.iessochoa.carlosarroyogalan.practica5.R
 
@@ -8,5 +11,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        //Busca la preferencia de version
+        val buildVersion: Preference? = findPreference("buildVersion")
+        //Se define una acción para la preferencia, añadiendo nuestra pagina
+        buildVersion?.setOnPreferenceClickListener {
+            startActivity(
+                Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://portal.edu.gva.es/03013224/"))
+            )
+            //Devuelve un booleano para confirmas el cambio
+            false
+        }
     }
 }
